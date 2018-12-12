81d52172d69b5f07a897b0a28d62ac9f66379073
Phoenix <phoenix986@gmail.com>
2018-12-12 18:35:14 +0100

    migrate to androidx update to gradle 4.6 update to gradle build tool 3.2.1 update to glide 4.8.0

--- zgallery/src/main/java/com/mzelzoghbi/zgallery/adapters/HorizontalListAdapters.java
+++ zgallery/src/main/java/com/mzelzoghbi/zgallery/adapters/HorizontalListAdapters.java
@@ -3,7 +3,7 @@ package com.mzelzoghbi.zgallery.adapters;
 import android.app.Activity;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
